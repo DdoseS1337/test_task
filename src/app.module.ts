ceac6b12522bc7e882834107d727f7c094d4db90
Yaroslav Maksymchuk <yaroslav.maksymchuk@gmial.com>
2023-12-28 22:10:41 +0200

    Create abstract database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { LoggerModule } from './logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, LoggerModule, ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      PORT: Joi.number().required(),
+      DATABASE_PORT: Joi.number().required(),
+      DATABASE_HOST: Joi.string().required(),
+      DATABASE_PASSWORD: Joi.string().required(),
+      DATABASE_USER: Joi.string().required(),
+      DATABASE_NAME: Joi.string().required(),
+    }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DATABASE_HOST'),
+                port: configService.get('DATABASE_PORT'),
+                username: configService.get('DATABASE_USER'),
+                password: configService.get('DATABASE_PASSWORD'),
+                database: configService.get('DATABASE_NAME'),
+                synchronize: true,
+                autoLoadEntities: true,
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+})
+export class DatabaseModule {
+    static forFeature(entities: EntityClassOrSchema[]) {
+        return TypeOrmModule.forFeature(entities);
+    }
+}
